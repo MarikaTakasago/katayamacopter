01e7700c7cafb4a40b3cc480d540d6f728694e36
MarikaaTakasago <marikanboumarch@yahoo.co.jp>
2022-06-28 14:14:59 +0900

    add ref_maker and obj_maker but didnt work well

--- katacop_main.m
+++ katacop_main.m
@@ -7,6 +7,8 @@ close all  %とりあえず全部閉じる
 %諸々load
 load 'param.mat'
 load 'models.mat'
+make_ref
+make_mpcobject
 
 %simulink
 sim('katayamacopter.slx') 
---
+++ make_mpcobject.m
@@ -0,0 +1,3 @@
+mpcelev = mpc(sys_elev,ts);
+mpcpitch = mpc(sys_pitch,ts);
+mpctravel = mpc(sys_travel,ts);
