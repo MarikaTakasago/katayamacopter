8ebbe79c8cc5d6be9438b7fb43c0bb880755d4fa
MarikaaTakasago <marikanboumarch@yahoo.co.jp>
2022-07-24 14:30:18 +0900

    edit make_ref

--- make_ref.m
+++ make_ref.m
@@ -10,22 +10,22 @@ t_travel = (0:ts_travel:end_time);
 
 % elevation
 x3_ref = zeros(length(t_elev),2); % 2rows:[t;x]
-x3_ref(:,1) = t; %input
+x3_ref(:,1) = t_elev; %input
 %x3_ref(:,2) = x3_target_angle;
 x3_ref(1:10,2) = elev_min; %(x3(elevation)_min)
 
 x4_ref = zeros(length(t_elev),2);
-x4_ref(:,1) = t;
+x4_ref(:,1) = t_elev;
 %x4_ref(:,2) = 0.2;
 
 % travel
 x5_ref = zeros(length(t_travel),2);
-x5_ref(:,1) = t;
+x5_ref(:,1) = t_travel;
 x5_ref(:,2) = x5_target_angle;
 x5_ref(1:10,2) = 0;
 
 x6_ref = zeros(length(t_travel),2);
-x6_ref(:,1) = t;
+x6_ref(:,1) = t_travel;
 one_sec = 1 / ts_travel;
 x6_ref(1:one_sec,2) = 0.01;
 
